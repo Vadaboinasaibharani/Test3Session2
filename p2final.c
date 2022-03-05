7668d925737e41931b505e29923c3d31456b2d24
VADABOINA SAI BHARANI <94587713+Vadaboinasaibharani@users.noreply.github.com>
2022-03-05 08:22:17 +0000

    p1final.test3

--- p2final.c
+++ p2final.c
@@ -1,1 +1,9 @@
 #include<stdio.h>
+{
+int num,den;
+};
+typedef _fraction Fraction
+Fraction input_fraction();
+Fraction Largest_fraction(Fraction f1, Fraction f2, Fraction f3)
+void output(Fraction f1, Fraction f2, Fraction f3, Fraction largest)
+
